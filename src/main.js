8bbba31da70fb324ad8489d9aedc3f51110ef15e
dez <godlyrics7@gmail.com>
2021-08-13 02:58:31 +0200

    Init i18n

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
+import i18n from './plugins/i18n'
 import vuetify from '@/plugins/vuetify'
 import examplestore from './store/examplestore'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
@@ -17,6 +21,7 @@ const store = new Vuex.Store({
 
 new Vue({
   el: '#app',
+  i18n,
   router,
   store,
   vuetify,
